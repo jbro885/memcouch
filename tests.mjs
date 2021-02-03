248bb95904c02896da99aa12c05700521da4b14d
Nathan Vander Wilt <nate@calftrail.com>
2021-02-03 14:26:53 -0800

    drop some TDD

--- tests.mjs
+++ tests.mjs
@@ -3,14 +3,40 @@ import { strict as assert } from 'assert';
 import Memcouch from "./memcouch.mjs";
 
 let db = new Memcouch();
+let arr, doc;    // reused below
 
-db.update({_id:'A', _rev:1, value:0})
-db.set({_id:'A', _rev:1, value:1})
+arr = Array.from(db.allDocs);
+assert.equal(arr.length, 0, "How could there be docs even??");
+
+db.update(doc = {_id:'A', _rev:1, value:0});
+arr = Array.from(db.allDocs);
+assert.equal(arr.length, 1, "Should contain one doc.");
+assert.equal(arr[0], doc, "Doc should have original identity.");
+
+db.set(doc = {_id:'A', _rev:1, value:1});
+arr = Array.from(db.allDocs);
+assert.equal(arr.length, 1, "Should still contain only one document.");
+assert.notEqual(arr[0].value, 0, "Doc should NOT have original content.");
+assert.equal(arr[0], doc, "Should contain new document object.");
+
+db.update({_id:'A', _rev:2, value:42});
+arr = Array.from(db.allDocs);
+assert.equal(arr.length, 1, "Should STILL contain only one document.");
+assert.equal(arr[0], doc, "Should still expose locally-edited doc.");
+
+db.expectUpdate('A', 3);
+arr = Array.from(db.allDocs);
+assert.equal(arr[0], doc, "The locally-edited content should abides.");
+db.update(doc = {_id:'A', _rev:3, value:43});
+arr = Array.from(db.allDocs);
+assert.equal(arr[0], doc, "The remote content should prevail now.");
+assert.equal(arr.length, 1, "And still only one document.");
+
 db.set({_id:'A', _rev:1, value:2})
-db.update({_id:'A', _rev:2, value:42})
 db.set({_id:'A', _rev:1, value:3})
 
 let g = db.allDocs;
 db.set({_id:'B', value:true})
-console.log(Array.from(g));
-console.log(Array.from(db.allDocs));
+
+
+console.log("which tests that do exist, they did all pass.");
